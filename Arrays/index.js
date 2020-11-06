35a6b5067797ace3fc337a89a2b05394e1f12898
Aman Bhardwaj <devproddeploy@gmail.com>
2020-11-06 23:30:17 +0530

    arrays

--- Arrays/index.js
+++ Arrays/index.js
@@ -1,4 +1,4 @@
-const arrays = [1,2,3,4,5];
+const arrays = [ 1, 2, 3, 4, 5 ];
 
 // // ADT
 
@@ -14,37 +14,35 @@ const arrays = [1,2,3,4,5];
 
 // indexInsertion(0, 69);
 
-// // DELETE 
+// // DELETE
 // // arrays.pop();      // Last element gone
 // // arrays.shift();     // First Element gone
 // // Deletion by index
-// const indexDeletion = index => arrays.splice(index, 1);    
+// const indexDeletion = index => arrays.splice(index, 1);
 
 // indexDeletion(4);
 
 // SEARCHING
-const search = x => {
-    
-    for(let i=0; i<arrays.length; i++){
-        if(x === arrays[i]){
-            return i;
-        }
-    }
-}
+const search = (x) => {
+	for (let i = 0; i < arrays.length; i++) {
+		if (x === arrays[i]) {
+			return i;
+		}
+	}
+};
 
-const improvedSearch = x => {
+const improvedSearch = (x) => {
+	for (let i = 0; i < arrays.length; i++) {
+		if (x === arrays[i]) {
+			if (i > 0) {
+				[ arrays[i], arrays[i - 1] ] = [ arrays[i - 1], arrays[i] ]; // swap
+			}
+			return i;
+		}
+	}
 
-    for(let i=0; i<arrays.length; i++){
-        if(x === arrays[i]){
-            if(i > 0){
-                [arrays[i], arrays[i-1]] = [arrays[i-1], arrays[i]];    // swap
-            }
-            return i;
-        }
-    }
-
-    return 'not found';
-}
+	return 'not found';
+};
 // Becomes O(1) after sometime as the key element keep on comming at the 0th index
 // console.log(improvedSearch(5));
 // console.log(improvedSearch(5));
@@ -54,179 +52,182 @@ const improvedSearch = x => {
 // console.log(improvedSearch(5));
 // console.log(improvedSearch(5));
 
-
 // Swap 2 vars by destructuring
 // let a=5, b=7;
 // console.log(a,b);
 // [a, b] = [b,a];
 // console.log(a,b);
 
-
 // BINARY Search Array MUST be Sorted
-const binarySearch = key => {
+const binarySearch = (key) => {
+	let low = 0,
+		high = arrays.length,
+		mid;
 
-    let low = 0, high = arrays.length, mid;
+	while (low <= high) {
+		console.log('I tried'); // only 2 times
 
-    while(low <= high){
-
-        console.log('I tried');     // only 2 times
-
-        mid = Math.floor((low + high)/2);
-        if(key === arrays[mid]){
-            return mid;
-        }
-        else if(key < arrays[mid]){
-            high = mid - 1;
-        }
-
-        else if(key > arrays[mid]){
-            low = mid + 1;
-        }
-    }
-}
+		mid = Math.floor((low + high) / 2);
+		if (key === arrays[mid]) {
+			return mid;
+		} else if (key < arrays[mid]) {
+			high = mid - 1;
+		} else if (key > arrays[mid]) {
+			low = mid + 1;
+		}
+	}
+};
 // console.log(binarySearch(3));
 
-const recursvieBinarySearch = (array, low , high, key) => {
-    
-    if(low <= high){
-        const mid = Math.floor((high + low) / 2);
-        if(key === array[mid]){
-            return mid;
-        }
-        else if(key < arrays[mid]){
-            return recursvieBinarySearch(array, low, mid - 1, key);
-        }
-        
-        else if(key > arrays[mid]){
-            return recursvieBinarySearch(array, mid + 1, high, key);
-        }
-    }
-    
-    return -1;
-}
+const recursvieBinarySearch = (array, low, high, key) => {
+	if (low <= high) {
+		const mid = Math.floor((high + low) / 2);
+		if (key === array[mid]) {
+			return mid;
+		} else if (key < arrays[mid]) {
+			return recursvieBinarySearch(array, low, mid - 1, key);
+		} else if (key > arrays[mid]) {
+			return recursvieBinarySearch(array, mid + 1, high, key);
+		}
+	}
+
+	return -1;
+};
 
 // console.log(recursvieBinarySearch(arrays, 0, arrays.length - 1, -2));
 
 const maximum = () => {
-    
-    // return Math.max.apply(this, arrays);
+	// return Math.max.apply(this, arrays);
 
-    return arrays.sort((a, b) => b-a)[0];
+	return arrays.sort((a, b) => b - a)[0];
 
-    // let max = Number.MIN_SAFE_INTEGER;
-    // arrays.forEach(element => {
-    //     if(element > max){
-    //         max = element;
-    //     }
-    // })
+	// let max = Number.MIN_SAFE_INTEGER;
+	// arrays.forEach(element => {
+	//     if(element > max){
+	//         max = element;
+	//     }
+	// })
 
-    // return max;
-}
+	// return max;
+};
 
 const minimum = () => {
-    // return Math.min.apply(this, arrays);
+	// return Math.min.apply(this, arrays);
 
-    return arrays.sort((a, b) => a-b)[0];
+	return arrays.sort((a, b) => a - b)[0];
 
-    // let min = Number.MAX_SAFE_INTEGER;
-    // arrays.forEach(element => {
-    //     if(element < min){
-    //         min = element;
-    //     }
-    // })
+	// let min = Number.MAX_SAFE_INTEGER;
+	// arrays.forEach(element => {
+	//     if(element < min){
+	//         min = element;
+	//     }
+	// })
 
-    // return min;
-
-}
+	// return min;
+};
 
 // console.log(maximum());
 // console.log(minimum());
 
 const sum = () => {
+	return arrays.reduce((acc, element) => acc + element, 0);
 
-    return arrays.reduce((acc, element) => acc + element,0);
+	// let sum = 0;
 
-    // let sum = 0;
+	// arrays.forEach(element => {
+	//     sum += element;
+	// });
 
-
-    // arrays.forEach(element => {
-    //     sum += element;
-    // });
-
-    return sum;
-}
+	return sum;
+};
 // console.log(sum());
 
-const reverse = () => arrays.reverse();     // changes original array
+const reverse = () => arrays.reverse(); // changes original array
 
 // console.log(arrays);
 // console.log(reverse());
 
 // SORTED ARRAYS
 const sortedInsertion = (arrays, x) => {
-    arrays.unshift(x)
-    return arrays.sort((a, b) => a - b);
+	arrays.unshift(x);
+	return arrays.sort((a, b) => a - b);
 };
 
 // console.log(sortedInsertion(arrays, 10));
 
-const isSorted = arrays => {
-
-    for(let i=0; i<arrays.length; i++){
-        if(arrays[i] > arrays[i+1]){
-            return false;
-        }
-    }
-    return true;
-}
+const isSorted = (arrays) => {
+	for (let i = 0; i < arrays.length; i++) {
+		if (arrays[i] > arrays[i + 1]) {
+			return false;
+		}
+	}
+	return true;
+};
 
 // console.log(isSorted([1,53,333,2533,533]));
 
 const mergeArrays = (arr1, arr2) => {
-    // return arr1.concat(arr2);
-    // return [...arr1, ...arr2];
+	// return arr1.concat(arr2);
+	// return [...arr1, ...arr2];
 
-    // no duplicates (UNIQUE)
-    const arr3 = [...arr1, ...arr2];
-    return arr3.filter((value, index) => {
-        // only first position taken into account
-        return arr3.indexOf(value) === index;
-    });
-}
+	// no duplicates (UNIQUE)
+	const arr3 = [ ...arr1, ...arr2 ];
+	return arr3.filter((value, index) => {
+		// only first position taken into account
+		return arr3.indexOf(value) === index;
+	});
+};
 
 // console.log(mergeArrays([1,2,3,4], [5,2,1,8]));
 
 // missing elements in an array wrt to another
 const missing = (arr1, arr2) => {
-    return arr1.filter((v, i) => {
-        if(!arr2.includes(v)){
-            return v;
-        }
-    });
-}
+	return arr1.filter((v, i) => {
+		if (!arr2.includes(v)) {
+			return v;
+		}
+	});
+};
 
 // console.log(missing([1,2,3,4,5], [2,3,4]));
 
 // Hashing  Duplicates
 const hashing = (arr) => {
-    const hash = [];
-    const duplicates = [];
+	const hash = [];
+	const duplicates = [];
 
-    for(let i=0; i<= 71; i++){
-        hash.push(0);
-    }
+	for (let i = 0; i <= 71; i++) {
+		hash.push(0);
+	}
 
-    arr.forEach((v, i) => {
-        ++hash[v];
-    })
+	arr.forEach((v, i) => {
+		++hash[v];
+	});
 
-    hash.forEach((v, i) => {
-        if(v > 1){
-            duplicates.push(i);
-        }
-    })
+	hash.forEach((v, i) => {
+		if (v > 1) {
+			duplicates.push(i);
+		}
+	});
 
-    return duplicates;
-}
+	return duplicates;
+};
 
-console.log(hashing([66,67,,69,69, 66, 68,69,70,71, 71, 71, 4,5,2,1,12,3,4,5]));
+console.log(hashing([ 66, 67, , 69, 69, 66, 68, 69, 70, 71, 71, 71, 4, 5, 2, 1, 12, 3, 4, 5 ]));
+
+const orderedPairs = () => {
+	const sum = 5;
+	const array = [ 1, 0, 4, 5, 2, 3, 9, 10 ];
+	let res = [];
+
+	for (let i = 0; i < array.length; i++) {
+		for (let j = i + 1; j < array.length; j++) {
+			const check = array[i] + array[j];
+			if (check === sum) {
+				res = [ ...res, { first: array[i], second: array[j] } ];
+			}
+		}
+	}
+
+	console.log(res);
+};
